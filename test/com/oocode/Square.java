ff1975ca3e6fbcb4f63bb34f82a5ed7b47300d89
ivan moore <ivan@tadmad.co.uk>
2010-12-01 17:55:32 +0000

    static imports and reordering args for clarity

--- test/com/oocode/Square.java
+++ test/com/oocode/Square.java
@@ -5,19 +5,22 @@ package com.oocode;
 import java.util.Set;
 
 import static com.oocode.PossibilitiesAbove.aboveCanBe;
+import static com.oocode.PossibilitiesBelow.belowCanBe;
+import static com.oocode.PossibilitiesToLeft.toLeftCanBe;
+import static com.oocode.PossibilitiesToRight.toRightCanBe;
 import static com.oocode.PossibleGuesses.*;
 
 public enum Square {
-    GuessedShip('*', true, aboveCanBe(SHIP_OR_WATER), PossibilitiesToRight.toRightCanBe(SHIP_OR_WATER), PossibilitiesToLeft.toLeftCanBe(SHIP_OR_WATER), PossibilitiesBelow.belowCanBe(SHIP_OR_WATER)),
-    Single('o', true, aboveCanBe(ONLY_WATER), PossibilitiesToRight.toRightCanBe(ONLY_WATER), PossibilitiesToLeft.toLeftCanBe(ONLY_WATER), PossibilitiesBelow.belowCanBe(ONLY_WATER)),
-    Centre('#', true, aboveCanBe(SHIP_OR_WATER), PossibilitiesToRight.toRightCanBe(SHIP_OR_WATER), PossibilitiesToLeft.toLeftCanBe(SHIP_OR_WATER), PossibilitiesBelow.belowCanBe(SHIP_OR_WATER)),
-    Top('^', true, aboveCanBe(ONLY_WATER), PossibilitiesToRight.toRightCanBe(ONLY_WATER), PossibilitiesToLeft.toLeftCanBe(ONLY_WATER), PossibilitiesBelow.belowCanBe(ONLY_SHIP)),
-    Bottom('v', true, aboveCanBe(ONLY_SHIP), PossibilitiesToRight.toRightCanBe(ONLY_WATER), PossibilitiesToLeft.toLeftCanBe(ONLY_WATER), PossibilitiesBelow.belowCanBe(ONLY_WATER)),
-    Left('<', true, aboveCanBe(ONLY_WATER), PossibilitiesToRight.toRightCanBe(ONLY_SHIP), PossibilitiesToLeft.toLeftCanBe(ONLY_WATER), PossibilitiesBelow.belowCanBe(ONLY_WATER)),
-    Right('>', true, aboveCanBe(ONLY_WATER), PossibilitiesToRight.toRightCanBe(ONLY_WATER), PossibilitiesToLeft.toLeftCanBe(ONLY_SHIP), PossibilitiesBelow.belowCanBe(ONLY_WATER)),
-    Water('~', false, aboveCanBe(SHIP_OR_WATER), PossibilitiesToRight.toRightCanBe(SHIP_OR_WATER), PossibilitiesToLeft.toLeftCanBe(SHIP_OR_WATER), PossibilitiesBelow.belowCanBe(SHIP_OR_WATER)),
-    OutOfBounds('X', false, aboveCanBe(SHIP_OR_WATER), PossibilitiesToRight.toRightCanBe(SHIP_OR_WATER), PossibilitiesToLeft.toLeftCanBe(SHIP_OR_WATER), PossibilitiesBelow.belowCanBe(SHIP_OR_WATER)),
-    Unknown('?', false, aboveCanBe(SHIP_OR_WATER), PossibilitiesToRight.toRightCanBe(SHIP_OR_WATER), PossibilitiesToLeft.toLeftCanBe(SHIP_OR_WATER), PossibilitiesBelow.belowCanBe(SHIP_OR_WATER));
+    GuessedShip('*', true, aboveCanBe(SHIP_OR_WATER), belowCanBe(SHIP_OR_WATER), toLeftCanBe(SHIP_OR_WATER), toRightCanBe(SHIP_OR_WATER)),
+    Single('o', true, aboveCanBe(ONLY_WATER), belowCanBe(ONLY_WATER), toLeftCanBe(ONLY_WATER), toRightCanBe(ONLY_WATER)),
+    Centre('#', true, aboveCanBe(SHIP_OR_WATER), belowCanBe(SHIP_OR_WATER), toLeftCanBe(SHIP_OR_WATER), toRightCanBe(SHIP_OR_WATER)),
+    Top('^', true, aboveCanBe(ONLY_WATER), belowCanBe(ONLY_SHIP), toLeftCanBe(ONLY_WATER), toRightCanBe(ONLY_WATER)),
+    Bottom('v', true, aboveCanBe(ONLY_SHIP), belowCanBe(ONLY_WATER), toLeftCanBe(ONLY_WATER), toRightCanBe(ONLY_WATER)),
+    Left('<', true, aboveCanBe(ONLY_WATER), belowCanBe(ONLY_WATER), toLeftCanBe(ONLY_WATER), toRightCanBe(ONLY_SHIP)),
+    Right('>', true, aboveCanBe(ONLY_WATER), belowCanBe(ONLY_WATER), toLeftCanBe(ONLY_SHIP), toRightCanBe(ONLY_WATER)),
+    Water('~', false, aboveCanBe(SHIP_OR_WATER), belowCanBe(SHIP_OR_WATER), toLeftCanBe(SHIP_OR_WATER), toRightCanBe(SHIP_OR_WATER)),
+    OutOfBounds('X', false, aboveCanBe(SHIP_OR_WATER), belowCanBe(SHIP_OR_WATER), toLeftCanBe(SHIP_OR_WATER), toRightCanBe(SHIP_OR_WATER)),
+    Unknown('?', false, aboveCanBe(SHIP_OR_WATER), belowCanBe(SHIP_OR_WATER), toLeftCanBe(SHIP_OR_WATER), toRightCanBe(SHIP_OR_WATER));
 
     private final boolean isShip;
     private final PossibilitiesAbove possibilitiesAbove;
@@ -26,7 +29,7 @@ public enum Square {
     private final PossibilitiesToLeft possibilitiesToLeft;
     private final char c;
 
-    Square(char representation, boolean isShip, PossibilitiesAbove possibilitiesAbove, PossibilitiesToRight possibilitiesToRight, PossibilitiesToLeft possibilitiesToLeft, PossibilitiesBelow possibilitiesBelow) {
+    Square(char representation, boolean isShip, PossibilitiesAbove possibilitiesAbove, PossibilitiesBelow possibilitiesBelow, PossibilitiesToLeft possibilitiesToLeft, PossibilitiesToRight possibilitiesToRight) {
         this.c = representation;
         this.isShip = isShip;
         this.possibilitiesAbove = possibilitiesAbove;
